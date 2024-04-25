0000bdf4db851d3520c7be602d679f2e42fd4f0d
hafiyb <hafiyb@gmail.com>
2024-04-25 13:17:12 +0800

    feat: consolidate fetch into page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,22 +3,27 @@ import DataWindow from './components/DataWindow';
 
 const fetchData = async (page = 0) => {
   // connection to local database
-  const connection = await mysql.createConnection({
-    //host: 'databases.000webhost.com',
-    //database: 'id22085626_sample_db',
-    //user: 'id22085626_admin',
-    //password: 'Mb45!b162'
-    host: 'localhost',
-    user: 'root',
-    database: 'sample_db',
-  });
+  let connection;
+  try {
+    connection = await mysql.createConnection({
+      //host: 'databases.000webhost.com',
+      //database: 'id22085626_sample_db',
+      //user: 'id22085626_admin',
+      //password: 'Mb45!b162'
+      host: 'localhost',
+      user: 'root',
+      database: 'sample_db',
+    });
 
-  // query to select all items from the items table
-  const [results, fields] = await connection.execute(
-    `SELECT * FROM ${`items`}`
-  );
+    // query to select all items from the items table
+    const [results, fields] = await connection.execute(
+      `SELECT * FROM ${`items`}`
+    );
 
-  return results;
+    return results;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const Page = async () => {
@@ -28,7 +33,7 @@ const Page = async () => {
   return (
     <main className='flex h-screen flex-col items-center justify-between py-12 md:px-24 sm:px-4'>
       <div className='flex flex-col max-h-full w-full'>
-        <h1 className='mb-2'>Ounch</h1>
+        <h1 className='mb-2 text-3xl'>Ounch</h1>
         <DataWindow mockData={mockData} />
       </div>
     </main>
